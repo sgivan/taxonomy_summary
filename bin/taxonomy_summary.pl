ba21898865c8a857f2fcadffa516cc7ae9062354
Scott A. Givan <sgivan@yahoo.com>
2015-07-29 14:28:09 -0500

    Lots of changes to fix functionality for lists > 200 IDs. Don't use linux commands like sort, uniq. Add taxmap functionality.

--- bin/taxonomy_summary.pl
+++ bin/taxonomy_summary.pl
@@ -27,8 +27,9 @@ use LWP::UserAgent;
 use HTTP::Request;
 use IO::File;
 use IO::Pipe;
+use URI;
 
-my ($debug,$verbose,$help,$infile,$outfile,$order,$class,$family,$species,$genus);
+my ($debug,$verbose,$help,$infile,$outfile,$order,$class,$family,$species,$genus,$print_taxmap,$dna);
 
 my $result = GetOptions(
     "debug"     =>  \$debug,
@@ -41,6 +42,8 @@ my $result = GetOptions(
     "family"    =>  \$family,
     "genus"     =>  \$genus,
     "species"   =>  \$species,
+    "taxmap"    =>  \$print_taxmap,
+    "dna"       =>  \$dna,
 
 );
 
@@ -53,6 +56,8 @@ sub help {
 
     say <<HELP;
 
+    Input file should be a list of NCBI sequence ID's, one per line.
+
     "debug"     =>  \$debug,
     "verbose"   =>  \$verbose,
     "help"      =>  \$help,
@@ -63,6 +68,8 @@ sub help {
     "family"    =>  \$family,
     "genus"     =>  \$genus,
     "species"   =>  \$species,
+    "taxmap"    =>  print a table of gene ID -> taxonomy
+    "dna"       =>  input list contains NCBI DNA ID's instead of protein ID's
 
 HELP
 
@@ -71,6 +78,7 @@ HELP
 $infile = 'infile' unless ($infile);
 $outfile = "outfile.$$" unless ($outfile);
 $species = 1 unless ( $class || $order || $family || $genus);
+my $db = $dna ? 'nucleotide' : 'protein';
 
 my $fh = new IO::File;
 my $outfh = new IO::File;
@@ -83,6 +91,10 @@ if ($outfh->open("> $outfile")) {
 }
 
 my $idstring = '';
+my @id = ();
+my %taxmap = ();
+my @tax_summary = ();
+my @line = ();
 if ($fh->open("< $infile")) {
 
     my $idcnt = 0;
@@ -97,27 +109,30 @@ if ($fh->open("< $infile")) {
     while (<$fh>) {
         chomp(my $val = $_);
         #$idstring .= $val . ",";
-        $idstring .= "&id=$val";
+        next unless ($val =~ /\d/); # will not include header
+        if ($val =~ /\w+?\|(.+?)\|/) {
+        #if ($val =~ /\w+?\|(.+?)\./) {
+            $val = $1;
+        }
+        $idstring .= "&id=$val" if ($val =~ /\w+/);
+        push(@id, $val);
         ++$idcnt;
-        last if ($debug && $idcnt >= 150);
+        last if ($debug && $idcnt >= 10);
     }
+    #say "idcnt = $idcnt";
 
     #chop($idstring);        
     #$idcnt = 201 if ($debug);
 
     my $ua = LWP::UserAgent->new();
-    #$ua->agent("taxonomy_summary");
     $ua->agent("eutils/taxonomy_summary");
     my $base = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
     if ($idcnt < 200) {
-#        my $ua = LWP::UserAgent->new();
-#        $ua->agent("taxonomy_summary");
-
-        #my $req = HTTP::Request->new(GET => "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=taxonomy&retmode=xml&id=$idstring");
-        #say $base . "efetch.fcgi?db=taxonomy&retmode=xml" . "$idstring" if ($debug);
-        say $base . "efetch.fcgi?db=protein&retmode=xml" . "$idstring" if ($debug);
-        #my $req = HTTP::Request->new(GET => $base . "efetch.fcgi?db=taxonomy&retmode=xml&id=$idstring");
-        my $req = HTTP::Request->new(GET => $base . "efetch.fcgi?db=protein&retmode=xml&id=$idstring");
+        my $uri = URI->new($base . "efetch.fcgi?db=$db&retmode=xml&id=$idstring");
+    #say $base . "efetch.fcgi?db=protein&retmode=xml" . "$idstring" if ($debug);
+        say $uri->canonical() if ($debug);
+        #my $req = HTTP::Request->new(GET => $base . "efetch.fcgi?db=protein&retmode=xml&id=$idstring");
+        my $req = HTTP::Request->new(GET => $uri->canonical());
 
         my $res = $ua->request($req);
 
@@ -130,7 +145,8 @@ if ($fh->open("< $infile")) {
     } else {
 
         my $url = $base . "efetch.fcgi";
-        my $url_params = "db=protein";
+        #my $url_params = "db=$db&rettype=xml&retmode=xml&";# including rettype changes return content to include sequence data
+        my $url_params = "db=$db&retmode=xml&";
 
         my $req = HTTP::Request->new(POST => $url);
         $req->content_type('application/x-www-form-urlencoded');
@@ -152,11 +168,12 @@ if ($fh->open("< $infile")) {
     $fh->close();
     if ($debug) {
         say "$idstring";
-        exit();
+#        exit();
     }
     my $pipe = new IO::Pipe;
     if ($species) {
 
+        say "species" if ($debug);
         my $pipe2 = new IO::Pipe;
         $pipe2->reader("xml_grep --strict --text_only --cond GBSeq_organism $outfile");
         my @species = <$pipe2>;
@@ -175,24 +192,55 @@ if ($fh->open("< $infile")) {
 
         close(TEMP);
 
-        $pipe->reader("sort tempfile.$$ | uniq -c | sort -g -k 1 -r");
+        $pipe->reader("sort tempfile.$$");
 
     } elsif ($genus) {
-        $pipe->reader("xml_grep --strict --text_only GBSeq_taxonomy $outfile | cut -f 4,5,6,7 -d ';' | sort | uniq -c | sort -g -k 1 -r");
+        say "genus" if ($debug);
+        $pipe->reader("xml_grep --strict --text_only --cond GBSeq_taxonomy $outfile | cut -f 4,5,6,7 -d ';'");
     } elsif ($family) {
-        $pipe->reader("xml_grep --strict --text_only GBSeq_taxonomy $outfile | cut -f 4,5,6 -d ';' | sort | uniq -c | sort -g -k 1 -r");
+        say "family" if ($debug);
+        $pipe->reader("xml_grep --strict --text_only --cond GBSeq_taxonomy $outfile | cut -f 4,5,6 -d ';'");
     } elsif ($order) {
-        $pipe->reader("xml_grep --strict --text_only GBSeq_taxonomy $outfile | cut -f 4,5 -d ';' | sort | uniq -c | sort -g -k 1 -r");
+        say "order" if ($debug);
+        #$pipe->reader("xml_grep --strict --text_only GBSeq_taxonomy $outfile | cut -f 4,5 -d ';' | sort | uniq -c | sort -g -k 1 -r");
+        $pipe->reader("xml_grep --strict --text_only --cond GBSeq_taxonomy $outfile | cut -f 4,5 -d ';'");
     } elsif ($class) {
-        $pipe->reader("xml_grep --strict --text_only GBSeq_taxonomy $outfile | cut -f 4 -d ';' | sort | uniq -c | sort -g -k 1 -r");
+        say "class" if ($debug);
+        $pipe->reader("xml_grep --strict --text_only --cond GBSeq_taxonomy $outfile | cut -f 4 -d ';'");
     }
-    for my $line (<$pipe>) {
+    @line = <$pipe>;
+    @taxmap{@id} = @line;
+#    if (1) {
+#        say "keys in taxmap: " . keys(%taxmap);
+#    }
+    @tax_summary = unique_count(\@line);
+    for my $line (@tax_summary) {
         print $line;
+    }
+    if ($print_taxmap) {
+        open(TM,">","taxmap.txt");
+        for my $tmkey (keys(%taxmap)) {
+            print TM $tmkey . "\t" . $taxmap{$tmkey};
+        }
+        close(TM);
     }
 } else {
     die "can't find '$infile' to read";
 }
 
 $outfh->close();
-unlink($outfile);
+#unlink($outfile) unless ($debug);
 unlink("tempfile.$$") if ($species);
+
+sub unique_count {
+    my $in = shift;
+    my @out = ();
+
+    my %tax = ();
+    for my $val (@$in) {
+        ++$tax{$val};
+    }
+    @out  = map ( $tax{$_} . "\t" . $_ , sort { $tax{$b} <=> $tax{$a} } keys(%tax)  );
+#    say @out;
+    return @out;
+}
