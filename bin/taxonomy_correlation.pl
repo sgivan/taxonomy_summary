843eca21d74338266140cb4a984f324af4cce508
Scott A. Givan <sgivan@yahoo.com>
2015-08-05 12:36:17 -0500

    Making script to calculate statistical significance of taxonomy_summary output. This version uses Statistics::RankCorrelation module.

---
+++ bin/taxonomy_correlation.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE:  taxonomy_correlation.pl
+#
+#        USAGE:  ./taxonomy_correlation.pl  
+#
+#  DESCRIPTION:  Calculates the statistical significance of the taxonomy summary output
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Dr. Scott A. Givan (sag), givans@missouri.edu
+#      COMPANY:  University of Missouri, USA
+#      VERSION:  1.0
+#      CREATED:  08/05/15 12:18:00
+#     REVISION:  ---
+#===============================================================================
+
+use 5.010;      # Require at least Perl version 5.10
+use autodie;
+use Getopt::Long; # use GetOptions function to for CL args
+use warnings;
+use strict;
+use Statistics::RankCorrelation;
+
+my ($debug,$verbose,$help);
+
+my $result = GetOptions(
+    "debug"     =>  \$debug,
+    "verbose"   =>  \$verbose,
+    "help"      =>  \$help,
+);
+
+if ($help) {
+    help();
+    exit(0);
+}
+
+
+#my $x = [ 8, 7, 6, 5, 4, 3, 2, 1 ];
+my $x = [ 8, 8, 8, 8, 8, 8, 8, 8 ];# total = 64
+#my $y = [ 2, 1, 5, 3, 4, 7, 8, 6 ];
+my $y = [ 48, 4, 3, 4, 1, 1, 2, 1 ];
+ 
+my $c = Statistics::RankCorrelation->new( $x, $y, sorted => 1 );
+ 
+my $n = $c->spearman;
+say "spearman: $n";
+#my $t = $c->kendall;
+#say "kendall: $t";
+my $m = $c->csim;
+say "csim: $m";
+
+#say "spearman: $n\nkendall: $t\ncsim: $m";
+
+sub help {
+
+    say <<HELP;
+
+
+HELP
+
+}
+
+
+
