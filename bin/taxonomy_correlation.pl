ab25a31a39a4a8718fc9dc59ee783e934aa4d0bc
Scott A. Givan <sgivan@yahoo.com>
2015-08-05 16:36:08 -0500

    This version calculates the Pearson correlation coefficient and generates text output of a Chi-Square test. However, I'd rather handle the Chi-Square calculation within the script, if possible.

--- bin/taxonomy_correlation.pl
+++ bin/taxonomy_correlation.pl
@@ -28,13 +28,16 @@ use Statistics::Descriptive;
 use Math::Random qw/ random_multinomial /;
 #use List::Util qw/ sum /;
 use List::Vectorize;
+use Statistics::ChiSquare;
 
-my ($debug,$verbose,$help);
+my ($debug,$verbose,$help,$infile,$outfile,$sig);
 
 my $result = GetOptions(
     "debug"     =>  \$debug,
     "verbose"   =>  \$verbose,
     "help"      =>  \$help,
+    "file:s"    =>  \$infile,
+    "sig:i"     =>  \$sig,
 );
 
 if ($help) {
@@ -43,17 +46,43 @@ if ($help) {
 }
 
 my $stats = Statistics::Descriptive::Full->new();
-my @y = ( 48, 4, 3, 4, 1, 1, 2, 1 );
+#my @y = ( 48, 4, 3, 4, 1, 1, 2, 1 );
+my @y = ();
+$sig = 95 unless ($sig);
+$infile = 'infile' unless ($infile);
+
+my @inlines = ();
+if (-e $infile) {
+    open(IN,"<",$infile);
+    @inlines = <IN>;
+    close(IN);
+} else {
+    say "input file, $infile, doesn't exist";
+    exit(1);
+}
+
+for my $line (@inlines) {
+    my @linevals = split /\t/, $line;
+    push(@y,$linevals[0]);
+}
 
 $stats->add_data(@y);
 
+if (len(\@y) <= 1) {
+    say "not enough elements in list";
+    exit(0);
+}
+
 my $master_sum = sum(\@y);
 my @p = ( (($master_sum/scalar(@y))/$master_sum) x scalar(@y));
-say "p: @p";
+#say "p: @p";
 say "master sum: $master_sum";
 # generate a random set of numbers that add up to a specific integer
 my @rmulti = sort {$b <=> $a} random_multinomial($master_sum,@p);
+say "input data: @y";
+say "chi square: " . chisquare(@y);
 say "random multinomial: @rmulti";
+say "chi square: " . chisquare(@rmulti);
 
 my @lsf = $stats->least_squares_fit(@rmulti);
 
@@ -63,10 +92,10 @@ my $cor = cor(\@y,\@rmulti,'pearson');
 
 say "cor: $cor";
 
-my $ttest = Statistics::TTest->new();
-$ttest->set_significance(90);
-$ttest->load_data(\@y,\@rmulti);
-say $ttest->output_t_test();
+#my $ttest = Statistics::TTest->new();
+#$ttest->set_significance($sig);
+#$ttest->load_data(\@y,\@rmulti);
+#say $ttest->output_t_test();
 
 sub help {
 
