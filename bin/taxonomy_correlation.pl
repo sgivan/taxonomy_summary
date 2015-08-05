00068e300e18fa42f3fcfc45199f5d52b84989d5
Scott A. Givan <sgivan@yahoo.com>
2015-08-05 15:51:18 -0500

    I tried writing my own algorithm to generate a random array of numbers that add up to a total, but then I found the method in Math::Random

--- bin/taxonomy_correlation.pl
+++ bin/taxonomy_correlation.pl
@@ -23,7 +23,10 @@ use autodie;
 use Getopt::Long; # use GetOptions function to for CL args
 use warnings;
 use strict;
-use List::Vectorize qw/ :stat /;
+use Statistics::TTest;
+use Statistics::Descriptive;
+use Math::Random qw/ random_multinomial /;
+use List::Util qw/ sum /;
 
 my ($debug,$verbose,$help);
 
@@ -38,15 +41,63 @@ if ($help) {
     exit(0);
 }
 
+my $stats = Statistics::Descriptive::Full->new();
 #my $x = [ 8, 7, 6, 5, 4, 3, 2, 1 ];
-my $x = [ 8, 8, 8, 8, 8, 8, 8, 8 ];# total = 64
-my $y = [ 2, 1, 5, 3, 4, 7, 8, 6 ];
+#my $x = [ 8, 8, 8, 8, 8, 8, 8, 8 ];# total = 64
+#my @x = ( 8, 8, 8, 8, 8, 8, 8, 8 );# total = 64
+#my $y = [ 2, 1, 5, 3, 4, 7, 8, 6 ];
+#my @x = random_uniform_integer(8,1,8);
 #my @y = ( 2, 1, 5, 3, 4, 7, 8, 6 );
-#my @y = ( 48, 4, 3, 4, 1, 1, 2, 1 );
- 
-my $cor = dist($x,$y,'pearson');
- 
-say "cor: $cor";
+my @y = ( 48, 4, 3, 4, 1, 1, 2, 1 );
+
+$stats->add_data(@y);
+
+my $master_sum = sum(@y);
+my @p = ( (($master_sum/scalar(@y))/$master_sum) x scalar(@y));
+say "p: @p";
+say "master sum: $master_sum";
+my @rmulti = sort {$b <=> $a} random_multinomial($master_sum,@p);
+say "random multinomial: @rmulti";
+
+# generate a random set of numbers that add up to a specific integer
+
+my $total = $master_sum;
+my $values = 8;
+my $rtotal;
+my @randoms = ();
+
+for (my $i = 0; $i < $values; ++$i) {
+    say "\ntotal: $total";
+    #my $rvalue = random_uniform_integer(1,0,$total-1);
+    #$total = 1 unless ($total);
+    my $rvalue;
+    if ($i == $values - 1) {
+        $rvalue = $master_sum - $rtotal;
+        $rvalue = 1 unless ($rvalue > 0);
+    } else {
+        $total = 1 if ($total - 1 <= 0);
+        say "total: $total";
+        $rvalue = random_uniform_integer(1, 1, $total);
+    }
+    say "rvalue: $rvalue";
+#    $rvalue = 1 if (!$rvalue);
+    push(@randoms, $rvalue);
+    $rtotal += $rvalue;
+    $total -= $rvalue;
+}
+
+@randoms = sort {$b <=> $a} @randoms;
+
+say "randoms: @randoms";
+say "rtotal: $rtotal";
+
+#say "x: @x";
+#say "y: @y";
+#
+#my $ttest = Statistics::TTest->new();
+#$ttest->set_significance(90);
+#$ttest->load_data(\@x,\@y);
+#say $ttest->output_t_test();
 
 sub help {
 
