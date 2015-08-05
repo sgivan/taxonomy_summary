196f0d1c672372ece77e0befdece9e523b780d71
Scott A. Givan <sgivan@yahoo.com>
2015-08-04 22:48:32 -0500

    Commented lines that were migrated to loopit_clusterblast.sh.

--- bin/cmd_taxonomy.sh
+++ bin/cmd_taxonomy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 bs="bsub -R 'span[hosts=1]' -o %J.o -e %J.e"
 
-stdout=$(splitter -sequence contig.nfa -outseq Csplit.fasta -size 250)
-echo $stdout
-mkdir -p clusterblast_v_ncbi
-cd clusterblast_v_ncbi
-ln -fs ../Csplit.fasta ./seqs
-stdout=$(clusterblast -f seqs -d nt -b blastn -B -a '-num_alignments 20 -num_descriptions 20' -p 2)
-echo $stdout
-cd ..
+#stdout=$(splitter -sequence contig.nfa -outseq Csplit.fasta -size 250)
+#echo $stdout
+#mkdir -p clusterblast_v_ncbi
+#cd clusterblast_v_ncbi
+#ln -fs ../Csplit.fasta ./seqs
+#stdout=$(clusterblast -f seqs -d nt -b blastn -B -a '-num_alignments 20 -num_descriptions 20' -p 2)
+#echo $stdout
+#cd ..
 stdout=$(bestblastparse.pl -f clusterblast_v_ncbi -b blastn -n 1)
 echo $stdout
 cat bestblast.tab | cut -f 8 | cut -f 1 -d ' ' > idlist.txt
