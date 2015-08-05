1793c567930e9a6ae583df5751cf7644edd05d11
Scott A. Givan <sgivan@yahoo.com>
2015-08-05 17:15:50 -0500

    Near final version

--- bin/taxonomy_correlation.pl
+++ bin/taxonomy_correlation.pl
@@ -23,12 +23,9 @@ use autodie;
 use Getopt::Long; # use GetOptions function to for CL args
 use warnings;
 use strict;
-use Statistics::TTest;
-use Statistics::Descriptive;
-use Math::Random qw/ random_multinomial /;
-#use List::Util qw/ sum /;
 use List::Vectorize;
-use Statistics::ChiSquare;
+use Statistics::ChisqIndep;
+use Math::Random qw/ random_multinomial /;
 
 my ($debug,$verbose,$help,$infile,$outfile,$sig);
 
@@ -37,7 +34,7 @@ my $result = GetOptions(
     "verbose"   =>  \$verbose,
     "help"      =>  \$help,
     "file:s"    =>  \$infile,
-    "sig:i"     =>  \$sig,
+    "sig:f"     =>  \$sig,
 );
 
 if ($help) {
@@ -45,10 +42,10 @@ if ($help) {
     exit(0);
 }
 
-my $stats = Statistics::Descriptive::Full->new();
+my $chi = Statistics::ChisqIndep->new();
 #my @y = ( 48, 4, 3, 4, 1, 1, 2, 1 );
 my @y = ();
-$sig = 95 unless ($sig);
+$sig = 0.05 unless ($sig);
 $infile = 'infile' unless ($infile);
 
 my @inlines = ();
@@ -66,8 +63,6 @@ for my $line (@inlines) {
     push(@y,$linevals[0]);
 }
 
-$stats->add_data(@y);
-
 if (len(\@y) <= 1) {
     say "not enough elements in list";
     exit(0);
@@ -76,26 +71,30 @@ if (len(\@y) <= 1) {
 my $master_sum = sum(\@y);
 my @p = ( (($master_sum/scalar(@y))/$master_sum) x scalar(@y));
 #say "p: @p";
-say "master sum: $master_sum";
+#say "master sum: $master_sum";
 # generate a random set of numbers that add up to a specific integer
 my @rmulti = sort {$b <=> $a} random_multinomial($master_sum,@p);
-say "input data: @y";
-say "chi square: " . chisquare(@y);
-say "random multinomial: @rmulti";
-say "chi square: " . chisquare(@rmulti);
+my @chidata = (\@y, \@rmulti);
+$chi->load_data(\@chidata);
+if ($verbose) {
+    say "input data: @y";
+    say "random multinomial: @rmulti";
+}
 
-my @lsf = $stats->least_squares_fit(@rmulti);
+#$chi->print_summary();
+#
+#say "";
+#
+#$chi->print_contingency_table();
 
-say "pearson correlation: " . $lsf[2];
-
-my $cor = cor(\@y,\@rmulti,'pearson');
-
-say "cor: $cor";
-
-#my $ttest = Statistics::TTest->new();
-#$ttest->set_significance($sig);
-#$ttest->load_data(\@y,\@rmulti);
-#say $ttest->output_t_test();
+#say "p-value: " . $chi->{p_value};
+if ($chi->{p_value} <= $sig) {
+    say "p-value: " . $chi->{p_value} . ", significant: yes" if ($verbose);
+    say $chi->{p_value} . "\tyes";
+} else {
+    say "p-value: " . $chi->{p_value} . ", signficant: no" if ($verbose);
+    say $chi->{p_value} . "\tno";
+}
 
 sub help {
 
